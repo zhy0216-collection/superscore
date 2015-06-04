b7bbd082feae4be81f635da8ae72a7504db8cfed
Yang <zhy0216@gmail.com>
2015-06-04 21:31:05 +0800

    try to make lambda

--- superscore/lambda.js
+++ superscore/lambda.js
@@ -1,4 +1,53 @@
+/*
+ * Author: Oliver Steele
+ * Copyright: Copyright 2007 by Oliver Steele.  All rights reserved.
+ * License: MIT License
+ * Homepage: http://osteele.com/javascripts/functional
+ * Created: 2007-07-11
+ * Version: 1.0.2
+ * 
+ * Modified by Yang
+ *
+ */
 
 // this file contain code from function.js
 // with some modify
 // the main responsibility is to turn string to function
+
+// consider use env?
+function lambda(exprString) {
+    'use strict'
+    var params = [];
+    var expr = exprString;
+    var sections = expr.split(/\s*->\s*/m);
+    if (sections.length > 1) {
+        while (sections.length) {
+            expr = sections.pop();
+            params = sections.pop().replace(/^\s*(.*)\s*$/, '$1').split(/\s*,\s*|\s+/m);
+            sections.length && sections.push('(function(' + params + '){return (' + expr + ')})');
+        }
+    } else if (expr.match(/\b_\b/)) {
+        params = '_';
+    } else {
+        // test whether an operator appears on the left (or right), respectively
+        var leftSection = expr.match(/^\s*(?:[+*\/%&|\^\.=<>]|!=)/m),
+            rightSection = expr.match(/[+\-*\/%&|\^\.=<>!]\s*$/m);
+        if (leftSection || rightSection) {
+            if (leftSection) {
+                params.push('$1');
+                expr = '$1' + expr;
+            }
+            if (rightSection) {
+                params.push('$2');
+                expr = expr + '$2';
+            }
+        } else {
+            var vars = exprString.replace(/(?:\b[A-Z]|\.[a-zA-Z_$])[a-zA-Z_$\d]*|[a-zA-Z_$][a-zA-Z_$\d]*\s*:|true|false|null|undefined|this|arguments|'(?:[^'\\]|\\.)*'|"(?:[^"\\]|\\.)*"/g, '').match(/([a-z_$][a-z_$\d]*)/gi) || []; // '
+            for (var i = 0, v; v = vars[i++];)
+                params.indexOf(v) >= 0 || params.push(v);
+        }
+    }
+    return new Function(params, 'return (' + expr + ')');
+};
+
+module.exports = lambda;
--- test/lambda.js
+++ test/lambda.js
@@ -1,2 +1,22 @@
+var _ = require("../superscore/lambda");
+var assert = require("assert");
 
 
+describe('lambda', function(){
+    it("->", function(){
+        assert.equal(_('x -> 2*x')(2), 4);
+        assert.equal(_('x, y -> 2*x+3*y')(2, 3), 13);
+        assert.equal(_('x -> y -> x+y')(2)(3), 5);
+    });
+
+    it("direct return value", function(){
+        assert.equal(_('2*x')(2), 4);
+        assert.equal(_('2*x*y')(2, 3), 12);
+    });
+
+
+
+
+
+
+})
